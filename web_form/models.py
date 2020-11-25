c581e54a1a44e9d653528774f01c99ba7b6b2f6f
Sergey Pakhtusov <pascal65536@gmail.com>
2020-11-25 13:14:28 +0700

    Update models.py

--- web_form/models.py
+++ web_form/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=30, verbose_name='Заголвок статьи')
+    title = models.CharField(max_length=30, verbose_name='Заголовок статьи')
     user_name = models.CharField(max_length=30, verbose_name='Имя пользователя')
     text = models.TextField(verbose_name='Текст')
     date_send = models.DateField(auto_now=True)
